2130ee09ec471d2a0a48de32377e9ef7fb34aa48
Bana Khusnan <banakhsn04@gmail.com>
2023-06-04 16:01:47 +0700

    menambah toast

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,19 +1,45 @@
-@if ($message = session('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ $message }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-@endif
-
 <!-- Toast with Placements -->
-<div class="bs-toast toast m-2 position-absolute bottom-0 end-0 bg-primary" role="alert" aria-live="assertive"
-    aria-atomic="true" data-delay="2000" id="myToast" style="z-index: 1000">
+<div class="bs-toast toast m-2 position-fixed bottom-0 end-0 bg-primary" role="alert" aria-live="assertive"
+    aria-atomic="true" data-delay="4000" id="myToast" style="z-index: 1000">
     <div class="toast-header">
         <i class="bx bx-bell me-2"></i>
-        <div class="me-auto fw-semibold">Bootstrap</div>
-        <small>11 mins ago</small>
+        <div class="me-auto fw-semibold" id="titleToast"></div>
+        <small>Baru Saja</small>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
-    <div class="toast-body">Fruitcake chocolate bar tootsie roll gummies gummies jelly beans cake.</div>
+    <div class="toast-body"></div>
 </div>
 <!-- Toast with Placements -->
+
+@if (session('dangerToast'))
+@push('js')
+<script>
+    const mytoast = document.getElementById('myToast')
+    mytoast.classList.replace('bg-primary', 'bg-danger')
+    var bsAlert = new bootstrap.Toast(mytoast);//inizialize it
+    const toastBody = mytoast.querySelector('.toast-body')
+    const titleToast = mytoast.querySelector('#titleToast')
+
+    titleToast.innerHTML = 'Kesalahan'
+    toastBody.innerHTML = '{{ session("dangerToast") }}'
+    bsAlert.show();//show it
+</script>
+@endpush
+@endif
+
+@if ($message = session('success'))
+@push('js')
+<script>
+    const mytoast = document.getElementById('myToast')
+    mytoast.classList.replace('bg-primary', 'bg-success')
+    var bsAlert = new bootstrap.Toast(mytoast);
+
+    const toastBody = mytoast.querySelector('.toast-body')
+    const titleToast = mytoast.querySelector('#titleToast')
+
+    titleToast.innerHTML = 'Berhasil'
+    toastBody.innerHTML = '{{ session("success") }}'
+    bsAlert.show();//show it
+</script>
+@endpush
+@endif
