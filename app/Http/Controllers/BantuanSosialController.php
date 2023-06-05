0f759c3b11e5f09fb7507148c94f31f8c9bc4a40
Bana Khusnan <banakhsn04@gmail.com>
2023-06-05 15:19:48 +0700

    menambah fungsi pendaftaran bansos

--- app/Http/Controllers/BantuanSosialController.php
+++ app/Http/Controllers/BantuanSosialController.php
@@ -19,13 +19,30 @@ class BantuanSosialController extends Controller
 
     public function pendaftaran()
     {
-        $datalUser = DetailUser::where('user_id', auth()->user()->id)->first();
+        $datailUser = DetailUser::where('user_id', auth()->user()->id)->first();
         $nameParts = explode(' ', auth()->user()->name);
 
         return view('public.pendaftaran', [
             'title' => 'Pendaftaran Bantuan Sosial',
-            'data' => $datalUser,
+            'data' => $datailUser,
             'name' => $nameParts,
         ]);
+    }
+
+    public function pendaftaranStore(Request $request)
+    {
+        $user = DetailUser::where('user_id', auth()->user()->id)
+                ->whereNotNull(['nik' ,'address', 'date_of_birth', 'no_handphone', 'job'])->count();
+
+        if($user === 0)
+        {
+            return redirect()->route('bansos.pendaftaran')->with('dangerToast', 'Data kamu belum lengkap, harap lengkapi terlebih dahulu');
+        }
+
+        DetailUser::where('user_id', auth()->user()->id)->update([
+            'status_pendaftaran' => 1,
+            'status_bansos' => 'proses',
+        ]);
+        return redirect()->route('bansos.informasi-bantuan')->with('success', 'Berhasil mendaftar bantuan sosial, harap tunggu konfirmasi dari admin.');
     }
 }
