24d5d825ee4440b113ba09a3b47009a1cec485d5
Bana Khusnan <banakhsn04@gmail.com>
2023-06-05 15:16:00 +0700

    membuat akun admin melalui seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Support\Facades\Hash;
+use Database\Seeders\DetailUserSeeder;
+use Database\Seeders\PermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +18,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([RoleSeeder::class, PermissionSeeder::class]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ])->assignRole('admin');
 
-        // $this->call([RoleSeeder::class, PermissionSeeder::class]);
-        $this->call([DetailUserSeeder::class]);
+        // $this->call([DetailUserSeeder::class]);
     }
 }
