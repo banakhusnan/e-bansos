7aace2bb75df841570f6c5c0fe6757d627eee29f
Bana Khusnan <banakhsn04@gmail.com>
2023-06-05 15:14:23 +0700

    menambah pesan error

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,7 +13,7 @@
             <!-- Account -->
             <div class="card-body">
                 <div class="d-flex align-items-start align-items-sm-center gap-4">
-                    <img src="{{ url('/sneat/assets/img/avatars/1.png') }}" alt="user-avatar" class="d-block rounded"
+                    <img src="{{ url('/assets/assets/img/avatars/1.png') }}" alt="user-avatar" class="d-block rounded"
                         height="100" width="100" id="uploadedAvatar" />
                     <div class="button-wrapper">
                         <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
@@ -39,38 +39,97 @@
                     @csrf
                     @method('put')
                     <div class="row">
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label for="firstName" class="form-label">First Name</label>
-                            <input class="form-control" type="text" id="firstName" name="firstName"
-                                value="{{ $name[0] }}" autofocus />
+                            <input class="form-control @error('firstName') is-invalid @enderror" type="text"
+                                id="firstName" name="firstName" value="{{ old('firstName', $name[0]) }}" autofocus />
+                            @error('firstName')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label for="lastName" class="form-label">Last Name</label>
-                            <input class="form-control" type="text" name="lastName" id="lastName"
-                                value="{{ implode(' ' ,array_slice($name, 1, count($name))) }}" />
+                            <input class="form-control @error('lastName') is-invalid @enderror" type="text"
+                                name="lastName" id="lastName"
+                                value="{{ old('lastName', implode(' ' ,array_slice($name, 1, count($name)))) }}" />
+                            @error('lastName')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label for="email" class="form-label">E-mail</label>
-                            <input class="form-control" type="text" id="email" name="email"
-                                value="{{ auth()->user()->email }}" placeholder="john.doe@example.com" />
+                            <input class="form-control @error('email') is-invalid @enderror" type="text" id="email"
+                                name="email" value="{{ old('email',auth()->user()->email) }}"
+                                placeholder="john.doe@example.com" />
+                            @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label class="form-label" for="no_handphone">Phone Number</label>
                             <div class="input-group input-group-merge">
-                                <span class="input-group-text">ID (+62)</span>
-                                <input type="text" id="no_handphone" name="no_handphone" class="form-control"
-                                    value="{{ substr($data->no_handphone, 3) }}" placeholder="85xxxxxxxxxx" />
+                                <span class="input-group-text @error('no_handphone') is-invalid @enderror">ID
+                                    (+62)</span>
+                                <input type="text" id="no_handphone" name="no_handphone"
+                                    class="form-control @error('no_handphone') is-invalid @enderror"
+                                    value="{{ old('no_handphone', !empty($data->no_handphone) ? substr($data->no_handphone, 3) : '' ) }}"
+                                    placeholder="85xxxxxxxxxx" />
+                                @error('no_handphone')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label for="date_of_birth" class="form-label">Date of Birth</label>
-                            <input type="date" class="form-control" id="date_of_birth" name="date_of_birth"
-                                value="{{ $data->date_of_birth }}" />
+                            <input type="date" class="form-control @error('date_of_birth') is-invalid @enderror"
+                                id="date_of_birth" name="date_of_birth"
+                                value="{{ old('date_of_birth', $data->date_of_birth ?? '') }}" />
+                            @error('date_of_birth')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
-                        <div class="mb-3 col-md-6">
+                        <div class="has-validation mb-3 col-md-6">
                             <label class="form-label" for="address">Address</label>
-                            <input type="text" class="form-control" id="address" name="address"
-                                value="{{ $data->address }}" placeholder="Address" />
+                            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address"
+                                name="address" value="{{ old('address',$data->address ?? '') }}"
+                                placeholder="Address" />
+                            @error('address')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="has-validation mb-3 col-md-6">
+                            <label class="form-label" for="address">NIK</label>
+                            <input type="text" class="form-control @error('nik') is-invalid @enderror" id="address"
+                                name="nik" value="{{ old('nik', $data->nik ?? '') }}" placeholder="NIK" />
+                            @error('nik')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="has-validation mb-3 col-md-6">
+                            <label class="form-label" for="address">Pekerjaan</label>
+                            <select name="job" class="form-select" id="">
+                                @foreach (['PNS', 'Karyawan Swasta', 'Petani'] as $item)
+                                @if (!empty($data->job) && $item === $data->job)
+                                <option selected>{{ $item }}</option>
+                                @else
+                                <option>{{ $item }}</option>
+                                @endif
+                                @endforeach
+                            </select>
                         </div>
                         <div class="mt-2">
                             <button type="submit" class="btn btn-primary me-2">Save changes</button>
@@ -81,4 +140,13 @@
         </div>
     </div>
 </div>
+
+@push('js')
+<script>
+    const span = document.querySelector('span.input-group-text')
+    if (span.classList.contains('is-invalid')) {
+        span.style.border = '1px solid #ff3e1d'
+    }
+</script>
+@endpush
 @endsection
