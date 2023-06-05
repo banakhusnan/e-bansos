74492889793e6a4f5b542c5f5a3602a8d6dfd608
Bana Khusnan <banakhsn04@gmail.com>
2023-06-05 15:14:57 +0700

    updated

--- resources/views/layouts/app-partials/navigation-bar.blade.php
+++ resources/views/layouts/app-partials/navigation-bar.blade.php
@@ -22,7 +22,7 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="{{ URL::asset('/sneat/assets/img/avatars/1.png') }}" alt
+                        <img src="{{ URL::asset('/assets/assets/img/avatars/1.png') }}" alt
                             class="w-px-40 h-auto rounded-circle" />
                     </div>
                 </a>
@@ -32,7 +32,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
-                                        <img src="{{ asset('/sneat/assets/img/avatars/1.png') }}" alt
+                                        <img src="{{ asset('/assets/assets/img/avatars/1.png') }}" alt
                                             class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </div>
